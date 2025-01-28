b45dda4e629c8cae95c9c318682145805aa4277e
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-28 23:27:59 +0530

    Maximum Sum of all Subarrays of size K

--- practice.java
+++ practice.java
@@ -1,21 +1,18 @@
-import java.util.Arrays;
-
-
-public class practice {
+class practice {
     public static void main(String[] args) {
-        int [] arr = {20,10,5,30,90};
-        Arrays.sort(arr);
-        
-        int k= 3;
-        int  n = arr.length;
+        int arr[] = { 10,20,30,40 };
+        int k = 2;
+        int n = arr.length;
         int max_sum = Integer.MIN_VALUE;
-        for(int i=0;i<=n-k;i++){
-            int current_sum =0;
-            for(int j=0; j<k;j++){
-                current_sum+=arr[i+j];
+
+        for (int i = 0; i <= n - k; i++) {
+            int current_sum = 0;
+            for (int j = 0; j < k; j++) {
+                current_sum += arr[i + j];
             }
-            max_sum=Math.max(current_sum, max_sum);
+            max_sum = Math.max(current_sum, max_sum);
         }
+
         System.out.println(max_sum);
     }
 }

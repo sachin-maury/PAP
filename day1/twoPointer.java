d2fbcc216f18cee782497f07b6a698502bd6fa5b
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-28 23:47:55 +0530

    twopointer

--- day1/twoPointer.java
+++ day1/twoPointer.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class sumOfTwoPointer {
+public class twoPointer {
 
     public static void main(String[] args) {
         int [] arr = {20,10,5,-1,30,90};

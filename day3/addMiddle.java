e91d949f2f33e14cd4684a693f32f28b4a6806f3
Sachin Maurya <kumarsachin67671@gmail.com>
2025-01-31 20:51:15 +0530

    Adding after a value

---
+++ day3/addMiddle.java
@@ -0,0 +1,53 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    
+    static Node insertAfter(Node head, int key,
+                                   int newData) {
+        Node curr = head;
+        
+        
+        while (curr != null) {
+            if (curr.data == key)
+                break;
+            curr = curr.next;
+        }
+
+        if (curr == null)
+            return head;
+        Node newNode = new Node(newData);
+        newNode.next = curr.next;
+        curr.next = newNode;
+        return head;
+    }
+
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+      
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(5);
+        head.next.next.next = new Node(6);
+
+        int key = 3, newData = 4;
+        head = insertAfter(head, key, newData);
+
+
+        printList(head);
+    }
+}
